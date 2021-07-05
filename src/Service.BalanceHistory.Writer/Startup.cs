34f95a1033eca2cb6b95d7520873e6b899fa12ac
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-07-05 18:07:30 +0300

    update

--- src/Service.BalanceHistory.Writer/Startup.cs
+++ src/Service.BalanceHistory.Writer/Startup.cs
@@ -60,8 +60,6 @@ namespace Service.BalanceHistory.Writer
 
                 endpoints.MapGrpcSchemaRegistry();
 
-                //endpoints.MapGrpcService<WalletBalanceUpdateOperationInfoService>();
-
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
